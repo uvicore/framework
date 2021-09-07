820c9c8809ad47434a83aa7acb01164f07361b24
Matthew Reschke <mail@mreschke.com>
2021-09-07 15:04:34 -0600

    Tag 0.1.22

--- uvicore/__init__.py
+++ uvicore/__init__.py
@@ -4,7 +4,7 @@ from uvicore.foundation.decorators import event, model, seeder, service, table, 
 
 
 # Uvicore version.  Also available in app.version
-__version__ = '0.1.11'
+__version__ = '0.1.22'
 
 # Core (non service provider based) singletons as globals
 ioc: contracts.Ioc = None
