b2e2ac911d50346aeb1b4484c65ad9a1e1880169
Matthew Reschke <mail@mreschke.com>
2023-04-16 11:32:55 -0600

    Fix for 0.1.25

--- bin/deploy-pypi.sh
+++ bin/deploy-pypi.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
+# Before you can publish, you need to setup a pypi token
+# which lives in ~/.pypirc
+
+# Then use that token in poetry
+# poetry config pypi-token.pypi <tokenhere>
+
 poetry publish
