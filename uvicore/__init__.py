35071ebf7e78ea437a5654d357da1c85dcbe05a0
Matthew Reschke <mail@mreschke.com>
2025-02-01 11:17:36 -0700

    v0.3.2 Python 3.12 support. Readme updates. Redis updates.

--- uvicore/__init__.py
+++ uvicore/__init__.py
@@ -5,7 +5,7 @@ from uvicore.foundation.decorators import event, job, model, seeder, service, ta
 
 
 # Uvicore version.  Also available in app.version
-__version__ = '0.3.1'
+__version__ = '0.3.2'
 
 # Foundational
 ioc: contracts.Ioc = None
