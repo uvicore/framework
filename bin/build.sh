ec2abcb595cf2fe245e286348e2d4f01d9ffe658
Matthew Reschke <mail@mreschke.com>
2025-09-28 15:24:42 -0600

    v0.3.17 locking dependencies of pypi build

--- bin/build.sh
+++ bin/build.sh
@@ -2,4 +2,10 @@
 
 # Ensure you bump up the version number in pyproject.toml AND in __init__.py
 
-poetry build
+# Using --locked ensures the build process uses the precise dependency versions
+# specified in poetry.lock, leading to more consistent and reproducible builds.
+# This is particularly important for production builds where you want to avoid
+# unexpected changes in dependencies that could introduce bugs or vulnerabilities.
+# It helps maintain stability and reliability in your application by ensuring
+# that the same versions of dependencies are used every time you build the project.
+poetry build --locked
