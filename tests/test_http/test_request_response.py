37621ceeb230a9d96f6e1fc785db01eb190278d0
Matthew Reschke <mail@mreschke.com>
2026-06-22 18:14:30 -0600

    Remove stale client method reference in tests

--- tests/test_http/test_request_response.py
+++ tests/test_http/test_request_response.py
@@ -4,21 +4,21 @@ from uvicore.support.dumper import dump
 
 
 @pytest.mark.asyncio
-async def test_http_module_importable(app1, client):
+async def test_http_module_importable(app1):
     """Test that http module is importable"""
     from uvicore import http
     assert http is not None
 
 
 @pytest.mark.asyncio
-async def test_request_class_exists(app1, client):
+async def test_request_class_exists(app1):
     """Test that Request class exists"""
     from uvicore.http import Request
     assert Request is not None
 
 
 @pytest.mark.asyncio
-async def test_guard_class_exists(app1, client):
+async def test_guard_class_exists(app1):
     """Test that Guard class exists"""
     from uvicore.http.routing import Guard
     assert Guard is not None
